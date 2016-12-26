40446d53389442bb950b11be158f9441d0128dc3
Giap Tran <txgvnn@gmail.com>
2016-12-26 22:52:41 +0700

    fixed bin/fn

--- bin/i3lock.sh
+++ bin/i3lock.sh
@@ -2,5 +2,5 @@
 scrot /tmp/screen_locked.png
 #convert /tmp/screen_locked.png -blur 2x2  -charcoal 1 -colorize 70%   /tmp/screen_locked.png
 convert /tmp/screen_locked.png -blur 3x3  -implode 2 -colorize 70%   /tmp/screen_locked.png
-convert /tmp/screen_locked.png -pointsize 50 -fill white -draw 'text 300,200 "Oh shit, I am locked"' /tmp/screen_locked.png
+convert /tmp/screen_locked.png -pointsize 50 -fill white -draw 'text 300,200 "TxGVNN"' /tmp/screen_locked.png
 i3lock -i /tmp/screen_locked.png
