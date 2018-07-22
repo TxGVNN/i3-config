3848aedde0f4b370c277ac5f4793791fb9895a5a
Giap Tran <txgvnn@gmail.com>
2018-07-22 11:50:25 +0700

    Sometimes i3clock can't lock (i3/i3lock#98)

--- bin/i3lock.sh
+++ bin/i3lock.sh
@@ -5,5 +5,7 @@ convert /tmp/screen_locked.png -blur 1x1  -implode 1 -colorize 20%   /tmp/screen
 #convert /tmp/screen_locked.png -pointsize 50 -fill white -draw 'text 300,200 "TxGVNN"' /tmp/screen_locked.png
 convert /tmp/screen_locked.png -scale 20% -scale 500% /tmp/screen_locked.png
 convert /tmp/screen_locked.png ~/.i3/bin/lock.png -gravity center -composite -matte /tmp/screen_locked.png
-i3lock -i /tmp/screen_locked.png
+if ! i3lock -i /tmp/screen_locked.png; then
+    return 1
+fi
 xset dpms force off
