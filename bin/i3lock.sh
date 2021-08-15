55e5555a3904863da92b1f1bb3cda8a62406cfe6
Giap Tran <txgvnn@gmail.com>
2021-08-15 23:22:24 +0700

    bin/i3lock.sh: use temp file

--- bin/i3lock.sh
+++ bin/i3lock.sh
@@ -1,11 +1,13 @@
 #!/bin/sh
-scrot /tmp/screen_locked.png
-convert /tmp/screen_locked.png -blur 2x2  -charcoal 1 -colorize 70%   /tmp/screen_locked.png
-convert /tmp/screen_locked.png -blur 1x1  -implode 1 -colorize 20%   /tmp/screen_locked.png
-#convert /tmp/screen_locked.png -pointsize 50 -fill white -draw 'text 300,200 "TxGVNN"' /tmp/screen_locked.png
-convert /tmp/screen_locked.png -scale 20% -scale 500% /tmp/screen_locked.png
-convert /tmp/screen_locked.png ~/.i3/bin/lock.png -gravity center -composite -matte /tmp/screen_locked.png
-if ! i3lock -i /tmp/screen_locked.png; then
+IMG=$(mktemp -u --suffix=.lock.png)
+scrot "$IMG"
+convert "$IMG" -blur 2x2  -charcoal 1 -colorize 70% "$IMG"
+convert "$IMG" -blur 1x1  -implode 1 -colorize 20% "$IMG"
+#convert "$IMG" -pointsize 50 -fill white -draw 'text 300,200 "TxGVNN"' "$IMG"
+convert "$IMG" -scale 20% -scale 500% "$IMG"
+convert "$IMG" ~/.i3/bin/lock.png -gravity center -composite -matte "$IMG"
+if ! i3lock -i "$IMG"; then
     return 1
 fi
 xset dpms force off
+rm "$IMG"
